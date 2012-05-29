e63af3000953dda58b792443827a28022da06982
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-29 09:54:33 +0000

    Vignette + MoveStack
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/ClassMoveStack.R
+++ pkg/move/R/ClassMoveStack.R
@@ -6,3 +6,14 @@ setClass(Class="MoveStack",
 			animala="vector",
 			speciesa="vector")
 )
+
+
+if (!isGeneric("moveStack")) {setGeneric("moveStack", function(x, ...) standardGeneric("moveStack"))}
+
+##Creating a stacked move object
+setMethod(f="moveStack", 
+          signature=c(x="data.frame"), 
+          definition = function(x){
+            return(res)##MoveStack object
+          }
+          )
