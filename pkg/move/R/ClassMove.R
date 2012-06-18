72b657f51f1299072caef65f891ee093e8ff7513
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-18 12:19:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/ClassMove.R
+++ pkg/move/R/ClassMove.R
@@ -185,7 +185,11 @@ setMethod(f="move",
 
 
 ###extract number of locations from Move
-if (!isGeneric("n.locs")) {setGeneric("n.locs", function(obj) standardGeneric("n.locs"))}
+if (!isGeneric("n.locs")) 
+{
+	setGeneric("n.locs", function(obj) standardGeneric("n.locs"))
+}
+
 setMethod("n.locs", "Move", function(obj){
             return(length(coordinates(obj)[ ,1]))
           }
