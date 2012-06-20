33ada79ac2555c1cae7943aa05eccbac58c6194b
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-20 07:11:44 +0000

    Web upd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/moveWEB/Updates/Entries/2012/5/31_Day_of_longboarding_files/31_Day_of_longboarding.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20120531
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('31_Day_of_longboarding_files/31_Day_of_longboardingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
