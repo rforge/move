9ee9158f784f4bbc7bf1444392eb41f89f4cca14
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-11 15:01:11 +0000

    raster 2 To Contour
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@158 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/ClassDBBMM.R
+++ pkg/move/R/ClassDBBMM.R
@@ -339,8 +339,8 @@ setMethod(f = "contour",
 
 ### Contour to SpatialLinesDataFrame conversion
 #if (!isGeneric("outerProbability")){
-setGeneric("raster2contour", function(x, ...){standardGeneric("raster2contour")})
-# #}
+setGeneric("raster2contour", function(x, ...){standardGeneric("raster2contour")}) #bart kami do we want to skip maxpixels? if not than there would be no reason to have a different function name to rasterToContour
+#}
 setMethod(f = "raster2contour",
           signature = c(x="RasterLayer"),
           definition = function(x, ...){
