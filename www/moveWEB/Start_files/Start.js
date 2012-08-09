bca80d22675fc569bbcadb0da973bd745e8c9374
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-09 12:52:36 +0000

    web upd 2/2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@217 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/moveWEB/Start_files/Start.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120809
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),reflection_0:new IWReflection({opacity:0.50,offset:2.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Start_files/StartMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
