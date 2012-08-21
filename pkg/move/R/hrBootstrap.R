166933cc68184b63652f5b146e17f91c7d531a9a
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-21 13:55:54 +0000

    circling namespace load fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@242 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/hrBootstrap.R
+++ pkg/move/R/hrBootstrap.R
@@ -1,5 +1,3 @@
-require(move)
-
 setGeneric("move2ade", function(x){standardGeneric("move2ade")})
 #setMethod("move2ade", signature=".MoveTrack", definition=function(x){ SpatialPointsDataFrame(coords=coordinates(x),data==data.frame(rep(rownames(x@idData), n.locs(x)))) })
 setMethod("move2ade", signature=".MoveTrackSingle", definition=function(x){ SpatialPointsDataFrame(coords=coordinates(x),data=data.frame(rep(rownames(x@idData), n.locs(x)))) })
