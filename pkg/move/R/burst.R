c8833804d891f4a6aa6591d8f23c879c8987f4c3
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-24 13:03:08 +0000

    formatting changes to burst.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@249 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/burst.R
+++ pkg/move/R/burst.R
@@ -31,45 +31,52 @@ setMethod("burst",
             burst(x = x, f = as.factor(f))
           })
 
-setMethod("burst", c(x = "Move", f = "character"), definition = function(x, f, ...) {
-    if (length(f) == 1) {
-        burst(x = x, f = do.call("$", list(x, f))[-n.locs(x)])
-    } else {
-        burst(x = x, f = as.factor(f))
-    }
-    
-})
-setMethod("[", signature(x = ".MoveTrackSingleBurst"), function(x, i, j, ...) {
-    if (!missing(i)) {
-	tmp<- x@burstId[i]
-        x@burstId <-tmp[-length(tmp)]
-    } else {
-        i <- T
-    }
-    if (missing(j)) 
-        j <- T
-    if (class(i) == "character") 
-        stop("Not sure if these methods work for class character")
-    if (class(j) == "character") 
-        stop("Not sure if these methods work for class character")
-    callNextMethod(x = x, i = i, j = j, ...)
-})
-setMethod("split", signature = c(x = ".MoveTrackSingleBurst", f = "missing"), definition = function(x, 
-    f, ...) {
-    
-    f <- c(0, cumsum(diff(as.numeric(factor(paste(as.character(x@burstId), is.na(x@burstId))))) != 
-        0))
-	f<-c(f, max(f))
-    res <- list()
-    for (i in unique(f)) 
-	    res[[i + 1]] <- x[f == i | c(0, f[-n.locs(x)]) == i, ]
-    names(res) <- as.character(unlist(lapply(lapply(res, slot, "burstId"), "[", 1)))
-    res<-lapply(res, as, sub("Burst", "", class(x)))
-    return(res)
-})
+setMethod("burst", 
+          signature=c(x = "Move", f = "character"), 
+          definition = function(x, f, ...) {
+            if (length(f) == 1) {
+                burst(x = x, f = do.call("$", list(x, f))[-n.locs(x)])
+            } else {
+                burst(x = x, f = as.factor(f))
+            }
+          })
+
+setMethod("[", 
+          signature(x = ".MoveTrackSingleBurst"), 
+          definition=function(x, i, j, ...) {
+            if (!missing(i)) {
+              tmp<- x@burstId[i]
+              x@burstId <-tmp[-length(tmp)]
+            } else {
+              i <- T
+            }
+            if (missing(j)) 
+              j <- T
+            if (class(i) == "character") 
+              stop("Not sure if these methods work for class character")
+            if (class(j) == "character") 
+              stop("Not sure if these methods work for class character")
+            callNextMethod(x = x, i = i, j = j, ...)
+          })
+
+setMethod("split", 
+          signature = c(x = ".MoveTrackSingleBurst", f = "missing"), 
+          definition = function(x, f, ...) {    
+            f <- c(0, cumsum(diff(as.numeric(factor(paste(as.character(x@burstId), is.na(x@burstId)))))!=0))
+          	f <- c(f, max(f))
+            res <- list()
+            for (i in unique(f)) 
+        	    res[[i + 1]] <- x[f == i | c(0, f[-n.locs(x)]) == i, ]
+            names(res) <- as.character(unlist(lapply(lapply(res, slot, "burstId"), "[", 1)))
+            res <- lapply(res, as, sub("Burst", "", class(x)))
+            return(res)
+          })
+
 setAs("MoveBurst", "Move", function(from) {
 # last id can be different since that one is not telling anything about a segment in this obj
-    if (length(unique(from@burstId)) != 1) 
+      if (length(unique(from@burstId)) != 1) 
         stop("Not one unique burst id method wont work")
-    new("Move", as(from, ".MoveGeneral"), as(from, ".MoveTrackSingle"))
-}) 
+      new("Move", 
+          as(from,".MoveGeneral"), 
+          as(from,".MoveTrackSingle"))
+    }) 
