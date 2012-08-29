006d612b3309edc6af54f84c919f99739708a053
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-29 12:11:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@258 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/inst/unitTests/runit.moveStack.R
+++ pkg/move/inst/unitTests/runit.moveStack.R
@@ -4,7 +4,7 @@ test.moveStack<-function()
 	b<-move(x=1:10,y=1:10,time=as.POSIXct(1:10, origin='1970-1-1'),proj=CRS('+proj=longlat'), 
 		animal="a")
 	checkIdentical(coordinates(a), coordinates(b))
-#	DEACTIVATED("Need to look what we want here")
+	DEACTIVATED("Need to look what we want here")
 	bb<-split(moveStack(list(a,b)))
 	aa<-list(unnamed=a,a=b)
 	row.names(aa[[2]])<-1:10
