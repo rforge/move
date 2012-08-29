29ee3fe3030ca44fa2b9559581e83029e5ebc384
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-29 09:36:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@257 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/inst/unitTests/runit.moveStack.R
+++ pkg/move/inst/unitTests/runit.moveStack.R
@@ -1,9 +1,14 @@
 test.moveStack<-function()
 {
 	a<-move(x=1:10,y=1:10,time=as.POSIXct(1:10, origin='1970-1-1'),proj=CRS('+proj=longlat'))
-	b<-move(x=1:10,y=1:10,time=as.POSIXct(1:10, origin='1970-1-1'),proj=CRS('+proj=longlat'), animal="a")
+	b<-move(x=1:10,y=1:10,time=as.POSIXct(1:10, origin='1970-1-1'),proj=CRS('+proj=longlat'), 
+		animal="a")
 	checkIdentical(coordinates(a), coordinates(b))
-	DEACTIVATED("Need to look what we want here")
-	checkEquals(split(moveStack(list(a,b))), list(unnamed=a,a=b))# one problem seems to be moveStack does not deal with missed fixes, the other the rownames of the data frame
+#	DEACTIVATED("Need to look what we want here")
+	bb<-split(moveStack(list(a,b)))
+	aa<-list(unnamed=a,a=b)
+	row.names(aa[[2]])<-1:10
+	row.names(bb[[2]])<-1:10
+	checkEquals(bb,aa)# one problem seems to be moveStack does not deal with missed fixes, the other the rownames of the data frame
 
 }
