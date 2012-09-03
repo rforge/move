69c0a62a4d423377eb63b2eae17fe31693c80abc
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-03 10:18:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@270 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/inst/extdata/createsRDataFile.R
+++ pkg/move/inst/extdata/createsRDataFile.R
@@ -1,9 +1,10 @@
 ##creates the 
 require(move)
 leroy <- move(system.file("extdata","leroy.csv", package="move"))
-ricky <- move(system.file("extdata","ricky.csv", package="move"))[1:300, ]
+ricky <- move(system.file("extdata","ricky.csv", package="move"))[, ]
 stack <- moveStack(list(leroy, ricky))
-leroydbbmm <- brownian.bridge.dyn(spTransform(leroy), dimSize=85, location.error=23)
-rickydbbmm <- brownian.bridge.dyn(spTransform(ricky), dimSize=65, location.error=23)
+leroydbbmm <- brownian.bridge.dyn(spTransform(leroy), dimSize=85, location.error=23, time.step=5)
+rickydbbmm <- brownian.bridge.dyn(spTransform(ricky), dimSize=65, location.error=23, time.step=5)
 dbbmmstack <- brownian.bridge.dyn(spTransform(stack), dimSize=85, location.error=23, ext=.3, time.step=600)
-save(leroy, ricky, stack, leroydbbmm, rickydbbmm, dbbmmstack, file="pkg/move/inst/extdata/move.RData")
+save(leroy, ricky, stack, leroydbbmm, rickydbbmm, dbbmmstack, file="move.RData")
+
