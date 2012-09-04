14b27da2aba380f877cb5b91757d1ae5820bf32f
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-04 19:10:46 +0000

    some comments about what unitchecks check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@277 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/inst/unitTests/runit.moveStack.R
+++ pkg/move/inst/unitTests/runit.moveStack.R
@@ -11,12 +11,12 @@ test.moveStack<-function()
 	row.names(bb[[2]])<-1:10
 	checkEquals(bb,aa)# one problem seems to be moveStack does not deal with missed fixes, the other the rownames of the data frame
 	row.names(d@idData)<-sub('a','A A', row.names(d@idData))
-	checkException(new('MoveStack', d , trackId=factor(sub('a','A A', as.character(d@trackId))), idData=d@idData))
-  checkException(validObject(d))
+	checkException(new('MoveStack', d , trackId=factor(sub('a','A A', as.character(d@trackId))), idData=d@idData))# track ids are no good names
+  checkException(validObject(d))#validity check needs to fail because of changed rownames
   
 	a<-move(x=1:10,y=1:10,time=as.POSIXct(1:10, origin='1970-1-1'),proj=CRS('+proj=longlat'),animal="AAA")
 	a2<-move(x=1:10,y=1:10,time=as.POSIXct(1:10, origin='1970-1-1'),proj=CRS('+proj=longlat'),animal="AAA")  
 	tmp<-options(warn=2)$warn
-	checkException(moveStack(list(a,a2)))
+	checkException(moveStack(list(a,a2)))# warn about duplicate ids
 	options(warn=tmp)
 }
