f173e18966068abc367b0dafd955aab2c6988461
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-18 21:55:19 +0000

    added setAs function for dBMvariance
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@292 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/AAClass.R
+++ pkg/move/R/AAClass.R
@@ -166,6 +166,8 @@ setClass(Class = "dBMvarianceStack", contains = c(".MoveTrackStack", "dBMvarianc
              stop("Number of coordinates does not match the number of means")
            return(TRUE)
          })
+setAs( "dBMvariance","data.frame", function(from){ return(data.frame(as(as(from,".MoveTrack"),"data.frame"), as(as(from,"dBMvarianceTmp"),"data.frame")))})
+setAs('dBMvarianceTmp','data.frame',function(from){data.frame(window.size=from@window.size, margin=from@margin, means=from@means, in.windows=from@in.windows, interest=from@interest)})
 
 
 setClass(Class = ".UDStack", contains = c("RasterStack"), 
