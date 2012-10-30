c63631e768b5a1129a0ecc10a9b1b5ca4a6f7748
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-30 13:51:42 +0000

    adjusted plotBurst and lineMidpoint code on the basis of the MoveLab Report from Fede
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@305 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/lineMidpoint.R
+++ pkg/move/R/lineMidpoint.R
@@ -3,25 +3,54 @@ setMethod(f = "lineMidpoint",
           signature="SpatialPointsDataFrame",
           definition=function(object){
             track <- coordinates(object)
-            if (!grepl("longlat",proj4string(object))) stop(paste("Coordinates need longlat projection! Projection is:",proj4string(object)))
+            if(!isLonLat(object)) stop(paste("Coordinates need longlat projection! Projection is:",proj4string(object)))
             if (nrow(track)<2) { ##make the point at the coordinate
               mid <- t(coordinates(track))
             } else {
-              if (nrow(track)==2) { ##make the point at the center of the line
-                mid <- (coordinates(track)[2,]-coordinates(track)[1,])*.5+coordinates(track)[1,]
+              if (nrow(track)==2) { ##make the point at the center of the line on a great circle
+                mid <- midPoint(coordinates(track)[1,], coordinates(track)[2,])
               }
               if (nrow(track)>2){
                 dists <- seglength(object)
                 
-                totalDist <- sum(dists)
+                #totalDist <- sum(dists)
                 cumsum <- cumsum(dists)
-                lcum <- list(cumsum)
-                
+                #lcum <- list(cumsum)
                 min <- which(cumsum(dists)>0.5*sum(dists))[1]
-                prop <- (cumsum(dists)[min]-sum(dists)/2)/cumsum(dists)[min]
-                mid <- (coordinates(track)[min+1,]-coordinates(track)[min,])*prop+coordinates(track)[min,]
+
+                if (sum(dists)==0) {min <- 0} else {min <- which(cumsum>0.5*sum(dists))[1]}       
+                if (min==0) {mid <- coordinates(track)[1,]} else {
+                 if (min==1){
+                   
+                    prop <- ((0.5*sum(dists)))/(cumsum(dists)[min])
+                    #####ADDING CALCULATION ALONG GREAT CIRCLE USING DESTPOINT FROM GEOSPHERE
+                    #mid <- coordinates(track)[min,] + prop*(coordinates(track)[min+1,]-coordinates(track)[min,])
+                    mid <- destPoint(p=coordinates(track)[min,], 
+                                     b=bearing(coordinates(track)[min,], coordinates(track)[min+1,]), 
+                                     d=prop*dists[min])
+                    
+                  }else{        
+                    prop <- (sum(dists)/2-(cumsum(dists)[min-1]))/(cumsum(dists)[min]-(cumsum(dists)[min-1]))
+                    #####ADDING CALCULATION ALONG GREAT CIRCLE USING DESTPOINT FROM GEOSPHERE
+                    #mid <- coordinates(track)[min,] + prop*(coordinates(track)[min+1,]-coordinates(track)[min,])
+                    mid <- destPoint(p=coordinates(track)[min,], 
+                                     b=bearing(coordinates(track)[min,], coordinates(track)[min+1,]), 
+                                     d=prop*dists[min]*1000) #seglength returns km,for lonlat=T,destPoint uses m
+                  }
+                }
               }
             }
-            midSP <- SpatialPoints(coords=t(as.data.frame(x=mid, row.names=NULL)), proj4string=CRS("+proj=longlat"))
+            midSP <- SpatialPoints(coords=as.data.frame(x=mid, row.names=NULL), proj4string=CRS("+proj=longlat"))
             #return(midSP)
           })
+
+
+# 
+# ***********************
+# data <- read.csv2("~/Documents/Programming/Rmove/Track_Segmentation_Letter/beh_movebank.csv", header=T, sep=";", dec=".")
+# data <- data[order(time=as.POSIXct(x=data$time, format="%Y-%m-%d %H:%M:%S",tz="UTC")),]
+# test <- move(x=data$xi, y=data$yi,time=as.POSIXct(x=data$time, format="%Y-%m-%d %H:%M:%S",tz="UTC"), data=data, proj=CRS("+proj=longlat"))
+# a <- move::burst(test,f=test$beh_code[-n.locs(test)])
+# plotBursts(a,add=F,breaks=5, pch=20)  
+# 
+# ***********************
--- pkg/move/R/plotBursts.R
+++ pkg/move/R/plotBursts.R
@@ -27,7 +27,8 @@ setMethod(f = "plotBursts",
             #        sizeLST  <- FUN(ll, object)} #think about what to enter for FUN
             #sizes  <- as.numeric(cut(unlist(sizeLST), breaks=breaks))/max(as.numeric(cut(unlist(sizeLST), breaks=breaks))) 
             sizes  <- as.numeric(cut(sizesdf, breaks=breaks))/max(as.numeric(cut(sizesdf, breaks=breaks)),na.rm=T)*2
-            df <- cbind(col, sizes, do.call('rbind', lapply(midLST, data.frame)))
+            #df <- cbind(col, sizes, do.call('rbind', lapply(midLST, data.frame)))
+            df <- cbind(col, sizes, data.frame(do.call(rbind, midLST)))
             colnames(df) <- c("color", "size","x","y")
             spdf <- SpatialPointsDataFrame(coords=do.call(rbind, midLST), data=df[,1:2], proj4string=CRS("+proj=longlat"))           
             
@@ -38,4 +39,5 @@ setMethod(f = "plotBursts",
               plot(coordinates(object), type="l", ...)
               res <- apply(df, MARGIN=1, function(x,...){points(x=x[3], y=x[4], cex=as.numeric(x['size']), col=x['color'],...)}, ...)
             }
+            #return(spdf)
             })
