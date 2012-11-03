649a39217b47087a13c5151465c4a10fcaeadbd7
marcosmolla <marcosmolla@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-03 11:28:57 +0000

    minor change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@307 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/hrBootstrap.R
+++ pkg/move/R/hrBootstrap.R
@@ -4,9 +4,10 @@ setMethod("hrBootstrap",
           signature=c(x="SpatialPoints"),
           definition=function(x,rep,plot,...){
             if (any(grepl('adehabitatHR', installed.packages()))) require(adehabitatHR) else stop("You need to install the adehabitatHR package to proceed")
-            j <- round(exp(log(10)*seq(log10(5),log10(n.locs(x)),by=0.1)))
+            if (class(x)=="SpatialPoints") nlocs <- length(x) else nlocs <- n.locs(x)
+            j <- round(exp(log(10)*seq(log10(5),log10(nlocs),by=0.1)))
             print(j)
-            locs <- lapply(j, function(js,coords) {replicate(n=rep,SpatialPoints(coords[sample(1:n.locs(x),size=js,replace=F),]) )}, coords=coordinates(x))
+            locs <- lapply(j, function(js,coords) {replicate(n=rep,SpatialPoints(coords[sample(1:nlocs,size=js,replace=F),]) )}, coords=coordinates(x))
             mcps <- lapply(locs, lapply, mcp.area, percent=95, plotit=F,unin=unin,unout=unout)
             quant <- lapply(lapply(mcps, unlist), quantile, probs=seq(0,1,.25))
             quantLines <- as.data.frame(do.call("rbind", quant))
@@ -18,7 +19,9 @@ setMethod("hrBootstrap",
             if (plot){
               plot(x=j, quantLines[,ncol(quantLines)], xlab="number of coordinates for mcp", ylab=paste("habitat area (",unout,")"), type="n", ylim=range(quantLines))
               #apply(quantLines, MARGIN=2, lines, x=j)
-              abline(h=as.numeric(mcp.area(move2ade(x), percent=95, plotit=F, unin=unin, unout=unout)),lty=7)
+              if (class(x)=="SpatialPoints"){
+              abline(h=as.numeric(mcp.area(x, percent=95, plotit=F, unin=unin, unout=unout)),lty=7)} else {
+              abline(h=as.numeric(mcp.area(move2ade(x), percent=95, plotit=F, unin=unin, unout=unout)),lty=7)}
               lapply(1:ncol(quantLines), function(i, j, b) lines(x=j, y=b[[1]][[i]],lty=b[[2]][[i]],col=b[[3]][[i]],lwd=2), j=j, b=b)
             }
             return(quantLines)
