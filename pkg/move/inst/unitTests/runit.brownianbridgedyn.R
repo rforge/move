e12e0d14103c96efc993d28b8ebfc7740c29f297
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-14 16:03:39 +0000

    compressed data files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@313 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/inst/unitTests/runit.brownianbridgedyn.R
+++ pkg/move/inst/unitTests/runit.brownianbridgedyn.R
@@ -1,5 +1,5 @@
 test.brownian.bridge.dyn<-function(){
-  data <- move(system.file("extdata","leroy.csv",package="move"))
+  data <- move(system.file("extdata","leroy.csv.gz",package="move"))
   checkException(brownian.bridge.dyn(object=leroy, location.error=23.5, dimSize=150, ext=.3, time.step=600))
   
   r <- raster(nrows=100, ncols=100, xmn=0, xmx=10)
--- pkg/move/inst/unitTests/runit.spTransform.R
+++ pkg/move/inst/unitTests/runit.spTransform.R
@@ -1,5 +1,5 @@
 test.spTransfrom<-function(){
-	data <- move(system.file("extdata","leroy.csv",package="move"))
+	data <- move(system.file("extdata","leroy.csv.gz",package="move"))
 	data2<-spTransform(x=d<-spTransform(data,center=T), CRSobj=(proj4string(data)))
 	checkEquals(data,data2)
 }
