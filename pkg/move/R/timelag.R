3341e4506d691dbccf6434836acca952f5474aa5
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-26 16:41:00 +0000

    fix to timelags
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@321 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/timelag.R
+++ pkg/move/R/timelag.R
@@ -1,9 +1,9 @@
 ###extract time.lag from Move
 if (!isGeneric("time.lag")) {setGeneric("time.lag", function(x, ...) standardGeneric("time.lag"))}
 setMethod("time.lag", ".MoveTrackSingle", function(x, ...){
-  return(as.numeric(diff(x@timestamps),...)) #one less than locations! we need a more elegant way than just adding a zero 
+  return(as.numeric(diff(timestamps(x)),...)) #one less than locations! we need a more elegant way than just adding a zero 
 })
 
-setMethod("time.lag", "MoveStack", function(x, ...){
-  return(lapply(split(x), time.lag))
+setMethod("time.lag", ".MoveTrackStack", function(x, ...){
+  return(lapply(lapply(split(timestamps(x), x@trackId), diff),as.numeric,...))
 })
