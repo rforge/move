fc3bcd426b6897202aae7858fcb9b813cc50d5c0
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-09 17:13:39 +0000

    made raster to contour return one sldf
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@354 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/raster2contour.R
+++ pkg/move/R/raster2contour.R
@@ -13,5 +13,9 @@ setMethod(f = "raster2contour",
           signature = c(x = ".UDStack"),
           definition = function(x, ...) {
             xx<-split(x)
-            lapply(xx, raster2contour, ...=...)
+            tmp<-lapply(xx, raster2contour, ...=...)
+	    x<-mapply(spChFIDs, tmp, mapply(paste,lapply(tmp,row.names), names(tmp), SIMPLIFY=F))
+	    tmp<-do.call('rbind',mapply('[[<-',MoreArgs=list(i='individual.local.identifier'),x=x, value=names(tmp)))
+	    return(tmp)
+
           })
