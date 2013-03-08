c674057243e272407f15daf4b1aefaf6ca31c83f
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-08 16:01:11 +0000

    made warning for units time.lage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@373 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/timelag.R
+++ pkg/move/R/timelag.R
@@ -4,6 +4,11 @@ setMethod("time.lag", ".MoveTrackSingle", function(x, ...){
   return(as.numeric(diff(timestamps(x)),...)) 
 })
 
-setMethod("time.lag", ".MoveTrackStack", function(x, ...){
+setMethod("time.lag", ".MoveTrackStack", function(x, units,...){
+	  if(missing(units)){
+		  warning('Units not specified this could lead to different units for the time differences between individuals')
   return(lapply(lapply(split(timestamps(x), x@trackId), diff),as.numeric,...))
+	  }else{
+  return(lapply(lapply(split(timestamps(x), x@trackId), diff),as.numeric,units=units,...))
+	  }
 })
