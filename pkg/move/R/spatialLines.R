bf3766f416bd05911bbcaba97dfb4ef21e399928
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-11 15:28:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@386 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/spatialLines.R
+++ pkg/move/R/spatialLines.R
@@ -1,4 +1,6 @@
-
 setAs("Move", "SpatialLines", function(from){
-      SpatialLines(list(Lines(list(Line(data)),rownames(data@idData))),proj4string=CRS(proj4string(data)))
+      SpatialLinesDataFrame(SpatialLines(list(Lines(list(Line(from)),rownames(from@idData))),proj4string=CRS(proj4string(from))), from@idData)
+})
+setAs("MoveStack", "SpatialLines", function(from){
+      SpatialLinesDataFrame(SpatialLines(mapply('Lines',lapply(lapply(split(as(from,'SpatialPoints'), from@trackId),Line), list) , rownames(from@idData)),proj4string=CRS(proj4string(from))), from@idData)
 })
