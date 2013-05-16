530946f24af55dd95c3039424f30356e2fa076a5
bart <bart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-16 14:02:30 +0000

    ensure aspect ratio for plots
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/move@396 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/move/R/plot.R
+++ pkg/move/R/plot.R
@@ -1,14 +1,14 @@
 setGeneric("plot") 
 setMethod(f = "plot", 
           signature = c(x=".MoveTrackSingle", y="missing"), 
-          function(x, y, ...){
-            plot(coordinates(x), ...)
+          function(x, y,asp=1, ...){
+            plot(coordinates(x), asp=asp,...)
           })
 
 setMethod(f = "plot", 
           signature = c(x=".MoveTrackStack", y="missing"), 
-          function(x, y, type='p',...){
-            plot(coordinates(x), type='n', ...)
+          function(x, y, type='p',asp=1, ...){
+            plot(coordinates(x), type='n',asp=asp, ...)
             if(type %in% c('p','o','b'))
               res <- points(x,...)
             if(type %in% c('l','o','b'))
@@ -17,8 +17,8 @@ setMethod(f = "plot",
 
 setMethod(f = "plot", 
           signature = c(x=".MoveTrackSingleBurst", y="missing"), 
-          function(x, y, type='p', ...){
-            plot(coordinates(x), type="n",...)
+          function(x, y, type='p',asp=1, ...){
+            plot(coordinates(x),asp=asp, type="n",...)
             if(type %in% c("p", "o", "b"))
               res <- points(x, ...)
             if(type %in% c("l", "o", "b"))
